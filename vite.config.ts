00ad875224aa67ce7a07d3c74c47c67ab42e656a
Axoloteera <axoloteera@outlook.com>
2025-08-02 20:02:10 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import monkey, { util } from 'vite-plugin-monkey';
+import AutoImport from 'unplugin-auto-import/vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    AutoImport({
+      imports: [
+          util.unimportPreset
+      ]
+    }),
+    monkey({
+      entry: 'src/main.ts',
+      userscript: {
+        icon: 'https://www.luogu.com.cn/favicon.ico',
+        namespace: 'github/luguo',
+        match: [
+            'https://*.luogu.com.cn/*',
+            'https://*.luogu.com/*'
+        ],
+      },
+    }),
+  ],
+});
